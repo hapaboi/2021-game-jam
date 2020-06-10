4204b833986afbdfb26faf0bd8451c83ead013a8
hapaboi <66650677+hapaboi@users.noreply.github.com>
2020-06-10 01:28:30 -1000

    Add files via upload

--- Move.cs
+++ Move.cs
@@ -9,6 +9,7 @@ public class Move : MonoBehaviour
     public bool isGrounded = false;
     public int Jumpsleft = 1;
     public Animator animator;
+    public float moving = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +20,13 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Rotate();
         Jump();
-        //Get horizontal input and move character
+        AnimatorMove();
+        //get horizontal input and move character
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
-        //set animator variable speed to movement value
-        animator.SetFloat("Speed", );
+        
     }
     void Jump()
     {
@@ -38,4 +40,31 @@ public class Move : MonoBehaviour
             Jumpsleft--;
         }
     }
+    void Rotate()
+    {
+        //ROTATION WITH Q/E
+        if (Input.GetAxis("Rotate") < 0)
+        {
+            transform.Rotate(Vector3.forward * 5f);
+        }
+        if (Input.GetAxis("Rotate") > 0)
+        {
+            transform.Rotate(Vector3.forward * -5f);
+        }
+    }
+    void AnimatorMove()
+    {
+        //check if character is grounded and running and change animator variable
+        if (Input.GetAxis("Horizontal") != 0 && isGrounded == true)
+        {
+            moving = 1;
+        }
+        else
+        {
+            moving = 0;
+        }
+        //set animator variable speed to movement value
+        animator.SetFloat("Speed", moving);
+    }
 }
+   
